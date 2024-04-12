a0073a39c5b55f0934a00c6a7c9f5eefcafda513
Toni Tenchev <tonitenchev93@gmail.com>
2024-04-12 08:18:48 +0100

    practice

---
+++ src/comp/body.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Body extends React.Component {
+    render() {
+        return (
+
+            <div>
+                <main>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti distinctio ipsam repellat ratione provident quia soluta quae, numquam at praesentium. Facere quibusdam ipsum nam nostrum. Distinctio illum obcaecati soluta assumenda.</p>
+                </main>
+
+                <footer>
+                    doneee
+                </footer>
+            </div>
+        )
+    }
+}
+export default Body;
---
+++ src/comp/heading.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Heading = (props) => {
+    return (
+        <header className="headerStyle">
+          <h1>Done big</h1>
+          <h2>Big done</h2>
+        </header>
+      );
+}
+
+export default Heading; 
